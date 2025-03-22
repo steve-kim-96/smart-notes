cc0332453d3691c05076f21e013ef496568ce8cf
steve-kim-96 <soungku@hotmail.com>
2025-03-22 17:16:25 +1300

    add zod validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import "./styles/App.scss";
 import { Notes } from "./features/Notes/components/Notes";
 import { Route, Routes } from "react-router-dom";
+import { Login } from "./features/Login/Login";
 
 export const App = () => {
   return (
     <>
       <Routes>
         <Route path="/" element={<Notes />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   );
