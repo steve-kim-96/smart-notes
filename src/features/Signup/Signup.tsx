a2bb57bbff9f20ef07f3105d148f528208de52d8
steve-kim-96 <soungku@hotmail.com>
2025-03-22 17:44:27 +1300

    create signup form

---
+++ src/features/Signup/Signup.tsx
@@ -0,0 +1,20 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, useForm } from "react-hook-form";
+import { signupFormSchema } from "./__helper__/formSchema";
+
+export const Signup = () => {
+  const {
+    register,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(signupFormSchema),
+  });
+  return (
+    <div className="signup-container">
+      <Form>
+        <input {...register("username")} />
+        {errors.username?.message && <p>{errors.username.message}</p>}
+      </Form>
+    </div>
+  );
+};
---
+++ src/features/Signup/__helper__/formSchema.ts
@@ -0,0 +1,18 @@
+import z from "zod";
+
+export const signupFormSchema = z
+  .object({
+    email: z.string().email().optional(),
+    username: z.string().min(3).max(20).optional(),
+    password: z
+      .string()
+      .min(8, "Password must be at least 8 characters long")
+      .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
+      .regex(/[a-z]/, "Password must contain at least one lowercase letter")
+      .regex(/[0-9]/, "Password must contain at least one number")
+      .regex(/[\W_]/, "Password must contain at least one special character"),
+  })
+  .refine((data) => data.email || data.username, {
+    message: "Either email or username is required",
+    path: ["email", "username"],
+  });
